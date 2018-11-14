11d10cde29f7cba84535d10ae07baff48aaf91f8
Swati_Rustogi <swati.rustogi@gmail.com>
2018-11-14 23:00:13 +0530

    Springboot AOP logger added

--- src/main/java/com/springboot/basics/aop/springaopfirst/SpringAopFirstApplication.java
+++ src/main/java/com/springboot/basics/aop/springaopfirst/SpringAopFirstApplication.java
@@ -1,12 +1,35 @@
 package com.springboot.basics.aop.springaopfirst;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringAopFirstApplication {
+import com.springboot.basics.aop.springaopfirst.business.Business1;
+import com.springboot.basics.aop.springaopfirst.business.Business2;
 
+@SpringBootApplication
+public class SpringAopFirstApplication implements CommandLineRunner {
+
+	@Autowired
+	private Business1 business1;
+	
+	@Autowired
+	private Business2 business2;
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAopFirstApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		logger.info(business1.calculateSomething());
+		logger.info(business2.calculateSomething());
+		
 	}
 }
